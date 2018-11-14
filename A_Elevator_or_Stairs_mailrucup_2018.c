177ca5361c29b581ace9d3461dbe0ca7dbe186ff
Sakib Hasan Nishat <39929993+sakib1207@users.noreply.github.com>
2018-11-14 20:54:40 +0600

    Add files via upload

--- A_Elevator_or_Stairs_mailrucup_2018.c
+++ A_Elevator_or_Stairs_mailrucup_2018.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+#include<math.h>
+int  main()
+{
+	int  x,y,z,t1,t2,t3,k=0,l=0,m=0,n=0;
+	scanf("%d%d%d%d%d%d",&x,&y,&z,&t1,&t2,&t3);
+	if(1<=x&&x<=1000&&1<=y&&y<=1000&&1<=z&&z <=1000&&1<=t1&&t1<=1000&&1<=t2&&t2<=1000&&1<=t3&&t3<=1000&&x!=y)
+	{
+		k=abs(y-x);
+		n=abs(x-z);
+        l=(k*t1);
+	    m=(3*t3)+((k+n)*t2);
+	if(l>=m)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO");
+	}
+	}
+	return 0;
+}
